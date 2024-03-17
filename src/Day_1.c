e57b9191984d9a136a2613ca5cd236d09d013097
Thomas Bocandé <thomasbocande@gmail.com>
2024-03-17 03:06:19 +0100

    pt2 day 1

--- src/Day_1.c
+++ src/Day_1.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void part1(char *data, size_t total_bytes);
+void part2(char *data, size_t total_bytes);
+
 int main() {
     FILE *file_ptr;
     char *data;
     size_t bytes_read;
     size_t total_bytes = 0;
     size_t buffer_size = 1000;
-    int sum = 0;
 
     file_ptr = fopen("inputs/input_day1", "r");
 
@@ -30,15 +32,35 @@ int main() {
 
     fclose(file_ptr);
 
+    part1(data, total_bytes);
+    part2(data, total_bytes);
+
+    free(data);
+
+    return 0;
+}
+
+void part1(char *data, size_t total_bytes) {
+    int sum = 0;
+
     for (size_t i = 0; i < total_bytes; i++) {
         if (data[i] == data[(i + 1) % total_bytes]) {
             sum += data[i] - '0';
         }
     }
 
-    printf("Sum: %d\n", sum);
+    printf("Part 1 result:\n%d\n", sum);
+}
 
-    free(data);
+void part2(char *data, size_t total_bytes) {
+    int sum = 0;
+    size_t half = total_bytes / 2;
 
-    return 0;
+    for (size_t i = 0; i < total_bytes; i++) {
+        if (data[i] == data[(i + half) % total_bytes]) {
+            sum += data[i] - '0';
+        }
+    }
+
+    printf("Part 2 result:\n%d\n", sum);
 }
